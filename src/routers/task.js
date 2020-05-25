8c05bddd0ea953f203bd59a67de4e932c50a84f7
aaron <aaron@MacBook-Pro.local>
2020-05-24 20:51:32 -0700

    API Task Management V3 - Code Clean Up and Promises Asynchronous Code.

--- src/routers/task.js
+++ src/routers/task.js
@@ -1,6 +1,45 @@
-const express = requires('express');
+const express = require('express');
 const Task = require('../models/tasks');
 const router = new express.Router();
+
+
+router.patch('/tasks/:id', async (req, res) => {
+    const updates = Object.keys(req.body);
+    const allowedUpdates = ['description', 'completed'];
+    const validOperation = updates.every((update) => {
+        return allowedUpdates.includes(update);
+    });
+
+    if (!validOperation){
+        return res.status(400).send({error: 'Invalid update!'});
+    }
+
+    try {
+         const task = await Task.findByIdAndUpdate(req.params.id, req.body, {new: true, runValidators: true});
+         if (!task) {
+             return res.status(404).send()
+         }
+         res.send(user)
+    } catch (e) {
+        res.status(400).send(e);
+    }
+});
+
+
+//Endpoint Handler, allows for removal of Tasks. Attempt to delete the Task by id. 
+// 1. Handle success 2. Handle task not found 3. Handle error
+router.delete('/tasks/:id', async (req, res) => {
+    try {
+        const task = await Task.findByIdAndDelete(req.params.id)
+
+        if (!task){
+            return res.status(404).send();
+        }
+        res.send(user);
+    }catch(e){
+        res.status(500).send();
+    }
+});
 
 
 router.post('/tasks', (req, res) => {
@@ -37,42 +76,5 @@ router.get('/tasks', (req, res) => {
     }
 });
 
-
-router.patch('/tasks/:id', async (req, res) => {
-    const updates = Object.keys(req.body);
-    const allowedUpdates = ['description', 'completed'];
-    const validOperation = updates.every((update) => {
-        return allowedUpdates.includes(update);
-    });
-
-    if (!validOperation){
-        return res.status(400).send({error: 'Invalid update!'});
-    }
-
-    try {
-         const task = await Task.findByIdAndUpdate(req.params.id, req.body, {new: true, runValidators: true});
-         if (!task) {
-             return res.status(404).send()
-         }
-         res.send(user)
-    } catch (e) {
-        res.status(400).send(e);
-    }
-});
-
-//Endpoint Handler, allows for removal of Tasks. Attempt to delete the Task by id. 
-// 1. Handle success 2. Handle task not found 3. Handle error
-router.delete('/tasks/:id', async (req, res) => {
-    try {
-        const task = await Task.findByIdAndDelete(req.params.id)
-
-        if (!task){
-            return res.status(404).send();
-        }
-        res.send(user);
-    }catch(e){
-        res.status(500).send();
-    }
-});
 
 module.exports = router;
--- src/routers/user.js
+++ src/routers/user.js
@@ -1,58 +1,6 @@
-const express = requires('express');
+const express = require('express');
 const User = require('../models/users');
 const router = new express.Router();
-
-
-router.post('/users', async (req, res) => {
-    const user = new User(req.body);
-    try{
-        await user.save();
-        res.status(201).send(user);
-    } catch (e) {
-        res.status(400).send(e);
-    }
-//    user.save().then(() => { 
- //       res.send(user);
- //   }).catch((e) => {
-//        res.status(400).send(e);
-//    });
-});
-
-router.get('/users', (req, res) =>{
-    try {
-        const users = await User.find({});
-        res.send(users)
-    } catch (e) {
-        res.status(500).send(e);
-    }
-//    User.find({}).then((users) => {
-//        res.send(users);
-//    }).catch((e) => {
-//        res.status(500).send(e);
-//    });
-});
-
-
-router.get('/users/:id', (req, res) => {
-    const idUser = req.params.id;
-    try {
-        const user = await User.findById({idUser});
-        if (!user){
-            return res.status(404).send();
-        }
-        res.send(user);
-    } catch (e) {
-        res.status(500).send(e);
-    }
-//    User.findById({idUser}).then((users) => {
-//        if (!user) {
-//            return res.status(404).send();
-//        }
-//        res.send(user);
-//    }).catch((e) => {
-//        res.status(500).send(e);
-//    });
-});
 
 
 router.patch('/users/:id', async (req, res) => {
@@ -77,6 +25,7 @@ router.patch('/users/:id', async (req, res) => {
     }
 });
 
+
 //Endpoint Handler, allows for removal of Users. Attempt to delete the User by id. 
 // 1. Handle success 2. Handle user not found 3. Handle error
 router.delete('/users/:id', async (req, res) => {
@@ -89,6 +38,41 @@ router.delete('/users/:id', async (req, res) => {
         res.send(user);
     }catch(e){
         res.status(500).send();
+    }
+});
+
+
+router.post('/users', async (req, res) => {
+    const user = new User(req.body);
+    try{
+        await user.save();
+        res.status(201).send(user);
+    } catch (e) {
+        res.status(400).send(e);
+    }
+});
+
+
+router.get('/users', (req, res) =>{
+    try {
+        const users = await User.find({});
+        res.send(users)
+    } catch (e) {
+        res.status(500).send(e);
+    }
+});
+
+
+router.get('/users/:id', (req, res) => {
+    const idUser = req.params.id;
+    try {
+        const user = await User.findById({idUser});
+        if (!user){
+            return res.status(404).send();
+        }
+        res.send(user);
+    } catch (e) {
+        res.status(500).send(e);
     }
 });
 
